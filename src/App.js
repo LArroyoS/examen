a80a08e9aea90743ec80ae36d50855ff8eceb997
LArroyoS <50588683+LArroyoS@users.noreply.github.com>
2022-05-06 11:55:57 -0500

    ListaProductos1.1

--- src/App.js
+++ src/App.js
@@ -5,50 +5,9 @@ import Menu from "./Menu/Menu.js";
 import About from "./Vistas/About";
 import Organigrama from "./Vistas/Organigrama";
 import Calculadora from "./Vistas/Calculadora";
+import ListaProductos from "./Vistas/ListaProductos";
 
 function App() {
-  const [listaProductos, setListaProductos] = useState([
-    {
-      nombre: "Producto1",
-      descripcion: "Descripcion",
-      precio: "0.00",
-      imagen: "./logo.png"
-    },
-    {
-      nombre: "Producto2",
-      descripcion: "Descripcion",
-      precio: "0.00",
-      imagen: "./logo.png"
-    },
-    {
-      nombre: "Producto2",
-      descripcion: "Descripcion",
-      precio: "0.00",
-      imagen: "./logo.png"
-    }
-  ]);
-  const ListaProd = () => {
-    return (
-      <table>
-        {
-          listaProductos.map((item) => {
-            return (
-              <tr>
-                <td>
-                  <img src={item.imagen} alt={item.nombre}/>
-                </td>
-                <td>
-                  <h4>{item.nombre}</h4>
-                  <h5>{item.descripcion}</h5>
-                  <h3><b>$ {item.precio}</b></h3>
-                </td>
-              </tr>
-            );
-          })
-        }
-      </table>
-    );
-  }
 
   return (
     <div className="App">
@@ -59,6 +18,7 @@ function App() {
         <About/>
         <Organigrama/>
         <Calculadora/>
+        <ListaProductos/>
       </section>
     </div>
   );
---
+++ src/Vistas/ListaProductos.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+function ListaProductos(){
+    const [listaProductos, setListaProductos] = useState([
+        {
+            nombre: "Producto1",
+            descripcion: "Descripcion",
+            precio: "0.00",
+            imagen: "./logo.png"
+        },
+        {
+            nombre: "Producto2",
+            descripcion: "Descripcion",
+            precio: "0.00",
+            imagen: "./logo.png"
+        },
+        {
+            nombre: "Producto2",
+            descripcion: "Descripcion",
+            precio: "0.00",
+            imagen: "./logo.png"
+        }
+    ]);
+    return (
+        <table>
+            {
+                listaProductos.map((item) => {
+                    return (
+                    <tr>
+                        <td>
+                        <img src={item.imagen} alt={item.nombre}/>
+                        </td>
+                        <td>
+                        <h4>{item.nombre}</h4>
+                        <h5>{item.descripcion}</h5>
+                        <h3><b>$ {item.precio}</b></h3>
+                        </td>
+                    </tr>
+                    );
+                })
+            }
+        </table>
+    );
+}
+
+export default ListaProductos;
